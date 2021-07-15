3b43ac92c18c7e6df51f0d7fb1dcee88e130b0c9
luffione00 <anhnguyenxuan099@gmail.com>
2021-07-15 13:05:25 +0700

    13:05

--- baitap1/databaitap1.sql
+++ baitap1/databaitap1.sql
@@ -26,3 +26,4 @@ insert into SanPham values('P102','SamsungNote10','SamSung',101);
 commit;
 
 select *from SanPham where masp='P100';
+select masp,tensp,nhasanxuat,tenloaisp from anhnx.SanPham s, anhnx.LoaiSP c where 1=1 and s.maloaisp=c.maloaisp;
